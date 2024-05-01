1468938b2996ade29d7c575c1aca0b6f2eb00036
SandriaRania <sandria.rania@ui.ac.id>
2024-05-01 11:58:33 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .compile(
-            &["proto/serevices.proto"], //Path to your proto file
+            &["proto/services.proto"], //Path to your proto file
             &["proto"],
         )?;
     Ok(())
